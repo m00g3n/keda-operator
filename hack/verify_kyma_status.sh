b3b31d957030762da36af926b3e850bbf9d22167
m00g3n <marcin.witalis@sap.com>
2022-11-23 11:23:39 +0100

    add coredns patching

--- hack/verify_kyma_status.sh
+++ hack/verify_kyma_status.sh
@@ -2,12 +2,12 @@
 
 function get_kyma_status {
 	local number=1
-	while [[ $number -le 20 ]] ; do
+	while [[ $number -le 12*3 ]] ; do
 		echo ">--> checking kyma status #$number"
 		local STATUS=$(kubectl get keda -n kcp-system default-kyma-keda -o jsonpath='{.status.state}')
-		echo ">----> kyma status: ${STATUS:='UNKNOWN'}"
+		echo "kyma status: ${STATUS:='UNKNOWN'}"
 		[[ "$STATUS" == "Ready" ]] && return 0
-		sleep 3
+		sleep 5
         	((number = number + 1))
 	done
 
